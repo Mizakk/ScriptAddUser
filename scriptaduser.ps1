191e7a9dd9cc4eead04c75af00e96ad32eb3474a
Valentine Micheli <valentine.micheli@ynov.com>
2021-03-12 21:31:06 +0100

    Update scriptaduser.ps1

--- scriptaduser.ps1
+++ scriptaduser.ps1
@@ -1,6 +1,6 @@
 # Script d'ajout d'utilisateurs avec un fichier .csv
 
-$Users = Import-Csv -Delimiter "," -Path "C:\Utilisateurs.csv"            
+$Users = Import-Csv -Delimiter "," -Path "C:\listeutilisateur.csv"            
 foreach ($User in $Users)            
 {            
     $Displayname = $User.Prenom + " " + $User.Nom            
@@ -8,13 +8,13 @@ foreach ($User in $Users)
     $UserLastname = $User.Nom
     $Prenom =  $UserFirstname.ToLower()
     $Nom =  $UserLastName.ToLower()   
-    $OU = "OU=" + $User.Departement + "," + "dc=Caslex,dc=local"      
+    $OU = "OU=" + $User.Departement + "," + "dc=VMI,dc=local"      
     $SAM = $Prenom+$Nom            
-    $UPN = $Prenom + "." + $Nom + "@" + "Caslex.local"
+    $UPN = $Prenom + "." + $Nom + "@" + "VMI.local"
     echo $UPN         
     $Departement = $User.Departement     
     $Password = $User.MotDePasse       
-    New-ADOrganizationalUnit -Name $User.Departement -Path "DC=CASLEX,DC=local" -ProtectedFromAccidentalDeletion $false
+    New-ADOrganizationalUnit -Name $User.Departement -Path "DC=VMI,DC=local" -ProtectedFromAccidentalDeletion $false
     New-ADUser -Name "$Displayname" -DisplayName "$Displayname" -SamAccountName $SAM -UserPrincipalName $UPN -EmailAddress $UPN -GivenName $UserFirstname -Surname $UserLastname -Description $Description -AccountPassword (ConvertTo-SecureString $Password -AsPlainText -Force) -Enabled $true -Path "$OU" -ChangePasswordAtLogon $false –PasswordNeverExpires $true     
 # La ligne 21 sert à ajouter un script d'ouverture de session. Ajouter un # au début de la ligne afin d'annuler ses effets sur le script.
     Get-ADUser -Filter * -SearchBase $OU | Set-ADUser –scriptPath “\\NOM-DU-SERVEUR\$Departement.bat”
